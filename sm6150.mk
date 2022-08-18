4a92546d7acf276f703b0ac4879e98e25e02b83a
zaidkhan0997 <kzaid0997@gmail.com>
2022-08-19 05:10:02 +0530

    sm6150-common: Welcome To Project-Zephyrus
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-zep
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
@@ -308,10 +308,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
-
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
 
 # Sensors
 PRODUCT_COPY_FILES += \
